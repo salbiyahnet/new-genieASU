59c1d70fc0b33bf9bb136bd8c242d88e528a6b84
salbiyahnet <99516182+salbiyahnet@users.noreply.github.com>
2025-05-20 11:15:40 +0700

    Update install24.sh

--- install24.sh
+++ install24.sh
@@ -3,59 +3,55 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 local_ip=$(hostname -I | awk '{print $1}')
+
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}===================== _ _ === _ ==========  _ =============  _ =============${NC}"
 echo -e "${GREEN}=========== ___  __ _| | |__ (_)_   _  __ _| |__  _ __   ___| |_  ==========${NC}"
-echo -e "${GREEN}===========/ __|/ _` | | '_ \| | | | |/ _` | '_ \| '_ \ / _ \ __|  =========${NC}"
-echo -e "${GREEN}===========\__ \ (_| | | |_) | | |_| | (_| | | | | | | |  __/ |_  ==========${NC}"
-echo -e "${GREEN}===========|___/\__,_|_|_.__/|_|\__, |\__,_|_| |_|_| |_|\___|\__| ==========${NC}"
+echo -e "${GREEN}===========/ __|/ _\` | | '_ \\| | | | |/ _\` | '_ \\| '_ \\ / _ \\ __|  =========${NC}"
+echo -e "${GREEN}===========\\__ \\ (_| | | |_) | | |_| | (_| | | | | | | |  __/ |_  ==========${NC}"
+echo -e "${GREEN}===========|___/\\__,_|_|_.__/|_|\\__, |\\__,_|_| |_|_| |_|\\___|\\__| ==========${NC}"
 echo -e "${GREEN}=============================== |___/  =====================================${NC}"
-echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================ Install GenieACS. =============================${NC}"
 echo -e "${GREEN}======================== NodeJS, MongoDB, GenieACS, ========================${NC}"
-echo -e "${GREEN}===================== By SALBIYAH-NET. Info 081336128448 =====================${NC}"
+echo -e "${GREEN}===================== By SALBIYAH-NET. Info 081336128448 ===================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-echo -e "${GREEN}Sebelum melanjutkan, silahkan baca terlebih dahulu. Apakah anda ingin melanjutkan? (y/n)${NC}"
+echo -e "${GREEN}Sebelum melanjutkan, apakah Anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
 
 if [ "$confirmation" != "y" ]; then
     echo -e "${GREEN}Install dibatalkan..${NC}"
-   
     exit 1
 fi
+
 for ((i = 5; i >= 1; i--)); do
-	sleep 1
+    sleep 1
     echo "Lanjut Boskuh... $i. Tekan ctrl+c untuk membatalkan"
 done
 
-#MongoDB
+# MongoDB Install
 if ! sudo systemctl is-active --quiet mongod; then
-echo -e "${GREEN}================== Menginstall MongoDB ==================${NC}"
-cd -
-sudo apt-get install gnupg curl
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod.service
-sudo systemctl start mongod
-sudo systemctl enable mongod
-mongo --eval 'db.runCommand({ connectionStatus: 1 })'
-echo -e "${GREEN}================== Sukses MongoDB ==================${NC}"
+    echo -e "${GREEN}================== Menginstall MongoDB ==================${NC}"
+    cd ~
+    sudo apt-get install gnupg curl -y
+    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo systemctl start mongod.service
+    sudo systemctl enable mongod
+    mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+    echo -e "${GREEN}================== Sukses MongoDB ==================${NC}"
 else
-    echo -e "${GREEN}============================================================================${NC}"
     echo -e "${GREEN}=================== mongodb sudah terinstall sebelumnya. ===================${NC}"
-    echo -e "${GREEN}============================================================================${NC}"
 fi
 
-#NodeJS Install
+# NodeJS Install
 check_node_version() {
     if command -v node > /dev/null 2>&1; then
         NODE_VERSION=$(node -v | cut -d 'v' -f 2)
         NODE_MAJOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 1)
         NODE_MINOR_VERSION=$(echo $NODE_VERSION | cut -d '.' -f 2)
-
         if [ "$NODE_MAJOR_VERSION" -lt 12 ] || { [ "$NODE_MAJOR_VERSION" -eq 12 ] && [ "$NODE_MINOR_VERSION" -lt 13 ]; } || [ "$NODE_MAJOR_VERSION" -gt 22 ]; then
             return 1
         else
@@ -73,26 +69,21 @@ if ! check_node_version; then
     sudo apt-get install -y nodejs npm
     rm nodesource_setup.sh
     echo "deb http://security.ubuntu.com/ubuntu impish-security main" | sudo tee /etc/apt/sources.list.d/impish-security.list
-    apt-get update
-    apt-get install libssl1.1
+    sudo apt-get update
+    sudo apt-get install -y libssl1.1
     echo -e "${GREEN}================== Sukses NodeJS ==================${NC}"
 else
     NODE_VERSION=$(node -v | cut -d 'v' -f 2)
-    echo -e "${GREEN}============================================================================${NC}"
     echo -e "${GREEN}============== NodeJS sudah terinstall versi ${NODE_VERSION}. ==============${NC}"
-    echo -e "${GREEN}========================= Lanjut install GenieACS ==========================${NC}"
-    echo -e "${GREEN}============================================================================${NC}"
-
 fi
 
-#GenieACS
-if !  systemctl is-active --quiet genieacs-{cwmp,fs,ui,nbi}; then
+# GenieACS Install
+if ! systemctl is-active --quiet genieacs-cwmp; then
     echo -e "${GREEN}================== Menginstall genieACS CWMP, FS, NBI, UI ==================${NC}"
     npm install -g genieacs@1.2.13
     useradd --system --no-create-home --user-group genieacs || true
-    mkdir -p /opt/genieacs
     mkdir -p /opt/genieacs/ext
-    chown genieacs:genieacs /opt/genieacs/ext
+    chown -R genieacs:genieacs /opt/genieacs
     cat << EOF > /opt/genieacs/genieacs.env
 GENIEACS_CWMP_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-cwmp-access.log
 GENIEACS_NBI_ACCESS_LOG_FILE=/var/log/genieacs/genieacs-nbi-access.log
@@ -102,13 +93,11 @@ GENIEACS_DEBUG_FILE=/var/log/genieacs/genieacs-debug.yaml
 GENIEACS_EXT_DIR=/opt/genieacs/ext
 GENIEACS_UI_JWT_SECRET=secret
 EOF
-    chown genieacs:genieacs /opt/genieacs/genieacs.env
-    chown genieacs. /opt/genieacs -R
     chmod 600 /opt/genieacs/genieacs.env
     mkdir -p /var/log/genieacs
-    chown genieacs. /var/log/genieacs
-    # create systemd unit files
-## CWMP
+    chown -R genieacs:genieacs /var/log/genieacs
+
+    # CWMP
     cat << EOF > /etc/systemd/system/genieacs-cwmp.service
 [Unit]
 Description=GenieACS CWMP
@@ -123,53 +112,53 @@ ExecStart=/usr/bin/genieacs-cwmp
 WantedBy=default.target
 EOF
 
-## NBI
+    # NBI
     cat << EOF > /etc/systemd/system/genieacs-nbi.service
 [Unit]
 Description=GenieACS NBI
 After=network.target
- 
+
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-nbi
- 
+
 [Install]
 WantedBy=default.target
 EOF
 
-## FS
+    # FS
     cat << EOF > /etc/systemd/system/genieacs-fs.service
 [Unit]
 Description=GenieACS FS
 After=network.target
- 
+
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-fs
- 
+
 [Install]
 WantedBy=default.target
 EOF
 
-## UI
+    # UI
     cat << EOF > /etc/systemd/system/genieacs-ui.service
 [Unit]
 Description=GenieACS UI
 After=network.target
- 
+
 [Service]
 User=genieacs
 EnvironmentFile=/opt/genieacs/genieacs.env
 ExecStart=/usr/bin/genieacs-ui
- 
+
 [Install]
 WantedBy=default.target
 EOF
 
-# config logrotate
- cat << EOF > /etc/logrotate.d/genieacs
+    # logrotate
+    cat << EOF > /etc/logrotate.d/genieacs
 /var/log/genieacs/*.log /var/log/genieacs/*.yaml {
     daily
     rotate 30
@@ -178,35 +167,37 @@ EOF
     dateext
 }
 EOF
-    echo -e "${GREEN}========== Install APP GenieACS selesai... ==============${NC}"
+
     systemctl daemon-reload
     systemctl enable --now genieacs-{cwmp,fs,ui,nbi}
-    systemctl start genieacs-{cwmp,fs,ui,nbi}    
-    echo -e "${GREEN}================== Sukses genieACS CWMP, FS, NBI, UI ==================${NC}"
+    echo -e "${GREEN}================== Sukses Install genieACS ==================${NC}"
 else
-    echo -e "${GREEN}============================================================================${NC}"
     echo -e "${GREEN}=================== GenieACS sudah terinstall sebelumnya. ==================${NC}"
 fi
 
-#Sukses
-echo -e "${GREEN}============================================================================${NC}"
-echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
-echo -e "${GREEN}=================== Informasi: Whatsapp 081-336-128-448 ====================${NC}"
-echo -e "${GREEN}============================================================================${NC}"
+# Info UI
+echo -e "${GREEN}========== GenieACS UI port 3000: http://$local_ip:3000 ============${NC}"
 
-echo -e "${GREEN}Sekarang install parameter. Apakah anda ingin melanjutkan? (y/n)${NC}"
+# Restore Mongo
+echo -e "${GREEN}Sekarang install parameter. Apakah Anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
 
 if [ "$confirmation" != "y" ]; then
     echo -e "${GREEN}Install dibatalkan..${NC}"
-    
     exit 1
 fi
+
 for ((i = 5; i >= 1; i--)); do
-	sleep 1
+    sleep 1
     echo "Lanjut Install Parameter $i. Tekan ctrl+c untuk membatalkan"
 done
 
-cd -
-sudo mongorestore --db=genieacs --drop new-genieASU
-rm -R new-genieASU
+cd ~
+
+if [ -d "new-genieASU" ]; then
+    sudo mongorestore --db=genieacs --drop new-genieASU
+    rm -rf new-genieASU
+    echo -e "${GREEN}Restore database selesai.${NC}"
+else
+    echo -e "${RED}Folder 'new-genieASU' tidak ditemukan. Restore database dibatalkan.${NC}"
+fi
